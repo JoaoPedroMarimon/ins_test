f04fc24ecb5f25eca797fa2aa3c348072115f90d
Henrique Patricio <he060190@intelbras.com.br>
2024-09-24 14:23:38 -0300

    fix wrong import of another ui file

--- src/IHM/src/view/limit_exceed_screen/limit_exceed.py
+++ src/IHM/src/view/limit_exceed_screen/limit_exceed.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QDialog
 
-from src.IHM.src.view.alert_screen.alert_screen_ui import Ui_Dialog
+from src.IHM.src.view.limit_exceed_screen.limit_exceed_screen_ui import Ui_Dialog
 
 class LimitExceed(QDialog, Ui_Dialog):
     def __init__(self):
