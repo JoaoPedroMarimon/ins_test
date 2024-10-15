46e77c12e2742fdaa6ecbe91ce46950f459da076
lu060428 <lu060428@intelbras.com.br>
2024-10-15 14:18:15 -0300

    feat: add product json into IHM

--- src/IHM/src/view/controller_view/controller_view.py
+++ src/IHM/src/view/controller_view/controller_view.py
@@ -1,17 +1,15 @@
 from PySide6.QtCore import QObject
 
-from src.IHM.src.components.communication.Enum.Inspetion_result import InspectionResult
 from src.IHM.src.view.first_screen.main_window import MainWindow
 from src.IHM.src.view.second_screen.second_screen import SecondScreen
 from src.IHM.src.components.communication.ihm_client import IHMClient
-from src.IHM.src.components.communication.controller_order import ControllerOrder
-from src.IHM.src.components.communication.Enum.Inspection_order import InspectionOrder
 
 
 class ControllerView(QObject):
-    def __init__(self):
+    def __init__(self,product_json:dict):
         super().__init__()
-        self.first_screen = MainWindow()
+        self.__product_json = product_json
+        self.first_screen = MainWindow(product_json=product_json)
         self.second_screen = SecondScreen()
         self.__config_server()
         self.signals_first_screen()
@@ -19,9 +17,10 @@ class ControllerView(QObject):
 
     def __config_server(self):
         self._server = IHMClient()
-        self._server.add_order_functions(self.get_switch_model, InspectionOrder.GET_MODEL)
-        for method in self.get_all_response_function_from_childs():
-            self._server.add_order_functions(method,InspectionResult.APROVADO)
+        self._server.OnReceiveResult.connect(self.second_screen.enum_to_history)
+        self._server.OnReceiveResult.connect(self.second_screen.mostrar_aprovado_reprovado)
+
+
 
     def show(self):
         self.first_screen.show()
@@ -44,13 +43,16 @@ class ControllerView(QObject):
     def switch_model_to_second_screen(self, switch_model: str) -> None:
         self.second_screen.set_name_switch(switch_model)
 
+    def get_model_index(self) -> int | None:
+        model_name = self.get_switch_model()
+        if model_name == 'MODELO SWITCH':
+            return None
+        return list(self.__product_json.keys()).index(model_name)
+
     def get_switch_model(self) -> str:
         return self.second_screen.get_name_switch()
 
-    def get_all_response_function_from_childs(self) -> list:
-        list_fun = []
-        list_fun += self.second_screen.get_on_response_functions()
-        return list_fun
+
     def on_close(self) -> None:
         self.first_screen.close()
         self.second_screen.close()
